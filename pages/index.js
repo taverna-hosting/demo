eb335c11e64c38be9af742e39608e0ce91f0f520
Oliver Lyman <oliver@tavernahosting.com>
2023-05-06 18:49:40 -0400

    Actually use the right imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import About from '../src/components/About';
 import Contact from '../src/components/Contact';
 import Footer from '../src/components/Footer';
-import Header from '../src/components/header';
-import Hours from '../src/components/hours';
+import Header from '../src/components/Header';
+import Hours from '../src/components/Hours';
 import Menus from '../src/components/Menus';
 import Reservations from '../src/components/Reservations';
 
